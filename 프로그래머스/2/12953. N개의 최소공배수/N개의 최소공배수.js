037d80fddc7af87797b8796a642a5cba07c6a133
1g2g <87280835+1g2g@users.noreply.github.com>
2023-12-20 20:19:05 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.26 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,20 +1,21 @@
 function solution(arr) {
-    const mul=[]
-    
-    arr.sort((a,b)=>a-b)
-    let i=arr[0]
-    while(i<arr[arr.length-1]){
-        let j=0
-        while(j<=arr.length-1){
-            const n=arr[j]
-            if(n%i===0){
-                arr[j]=n/i
-                if(mul[mul.length-1]!==i) mul.push(i)
-            }
-            j++
-        }
-        if(arr.every(n=>n%i!==0)) i++
+  let mul = 1;
+  let i = 2;
+
+  arr.sort((a, b) => a - b);
+  const max = arr[arr.length - 1];
+
+  while (i <= max) {
+    let j = 0;
+    if (arr.some((n) => n % i === 0)) mul *= i;
+    while (j <= arr.length - 1) {
+      const n = arr[j];
+      if (n % i === 0) {
+        arr[j] = n / i;
+      }
+      j++;
     }
-    
-    return arr.reduce((acc,num)=>acc*num,1)*mul.reduce((acc,num)=>acc*num,1);
+    if (arr.every((n) => n % i !== 0)) i++;
+  }
+  return arr.reduce((acc, num) => acc * num, 1) * mul;
 }
